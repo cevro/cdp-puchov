6075f10329469ce3f7d05fdbb30751f1e81d1bd8
Michal "Mišo" Červeňák <michalcerv363@gmail.com>
2019-07-24 08:44:11 +0200

    remove JS files

--- definitions/interfaces.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.MESSAGE_ACTION_STATE_UPDATE = 'state-update';
-exports.MESSAGE_ACTION_DUMP = 'dump';
-exports.MESSAGE_ACTION_MESSAGE = 'message';
--- definitions/messages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var MESSAGE_ACTION_STATE_UPDATE_GLOBAL = 'state-update';
-var ABSectorMessages;
-(function (ABSectorMessages) {
-    /* *********** client->server ************************/
-    /* *********** server->client ************************/
-    ABSectorMessages.MESSAGE_ACTION_STATE_UPDATE = MESSAGE_ACTION_STATE_UPDATE_GLOBAL;
-})(ABSectorMessages = exports.ABSectorMessages || (exports.ABSectorMessages = {}));
